c7406b20986245803d5c81945d8a635c8ba7885f
yanggang <yanggang_1997@foxmail.com>
2024-03-04 15:54:48 +0800

    初始化提交

--- logback-monitor-view/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Application from "./App";
-import 'moment/locale/zh-cn'
-import zhCN from 'antd/locale/zh_CN';
-import {ConfigProvider} from "antd";
-
-ReactDOM.render(<ConfigProvider locale={zhCN}><Application/></ConfigProvider>, document.getElementById('root'));
--- logback-monitor-view/src/layout/breadcrumb.js
+++
@@ -1,9 +0,0 @@
-import {Breadcrumb} from 'antd';
-import React from 'react';
-
-export default function AppBreadcrumb({style, currNav}) {
-    console.log("渲染面包屑：", currNav)
-    return <Breadcrumb style={style}>
-        {currNav.map(item => <Breadcrumb.Item key={"Breadcrumb_" + item.key}>{item.label}</Breadcrumb.Item>)}
-    </Breadcrumb>;
-}
